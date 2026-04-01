6a0b3f3fbb094395552bd45f16eb8f864b626db0
Aany-nil <nilporidreamgirl123@gmail.com>
2026-04-02 01:24:25 +0600

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,12 @@
+const registration = async (req, res)=>{
+    // const { fullName, email, password } = req.body
+
+    try {
+        res.send("registration successfully")
+    } catch (error) {
+        
+    }
+}
+
+
+module.exports = { registration }
---
+++ route/authRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { registration } = require("../controllers/authController");
+const router = express.Router();
+
+
+router.get("/registration", registration);
+
+
+module.exports = router;
